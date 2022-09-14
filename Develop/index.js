5da9162bfdf1e4b3e23b4e7ecd37f5a9da03f10c
dgraham742 <106363652+dgraham742@users.noreply.github.com>
2022-09-14 10:18:13 -0500

    First commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,26 @@
 // TODO: Include packages needed for this application
 
+const { default: inquirer } = require("inquirer");
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    
+    return inquirer.prompt([
+        {
+            type:'input',
+            name:'name',
+            message: 'What is your name?'
+        },
+
+])
+}
+  
+
+
+
+  
+
+;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
